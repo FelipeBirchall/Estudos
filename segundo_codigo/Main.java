4b10c9ec2bd2f72d1cc23dbd4c861c1ed7943fa5
Felipe Birchal Borges <felipebirchalborges@gmail.com>
2025-02-06 17:08:16 -0300

    .

--- segundo_codigo/Main.java
+++ segundo_codigo/Main.java
@@ -1,10 +1,23 @@
 package segundo_codigo;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
-        System.out.println("\nHello world!");
+        int N = scanner.nextInt();
+        int n = N;
+        int[] notas = {100, 50, 20, 10, 5, 2, 1};
+
+        System.out.println(n); 
+
+        for (int nota : notas) {
+            int quantidade = N / nota;
+            N %= nota; 
+
+            System.out.println(quantidade + " nota(s) de R$ " + nota + ",00");
+        }
+
+        scanner.close();
     }
-    
 }
---
+++ segundo_codigo/README.md
@@ -0,0 +1,3 @@
+## Decomposição de Valor em Cédulas usando for-each
+
+Este programa lê um valor inteiro e calcula a menor quantidade possível de notas necessárias para representar esse valor. Ele considera as cédulas de R$ 100, 50, 20, 10, 5, 2 e 1.

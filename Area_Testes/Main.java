99ad820a97febaa5a060015dcf01f96d45db58aa
Felipe Birchal Borges <felipebirchalborges@gmail.com>
2025-02-07 21:27:53 -0300

    .

--- Area_Testes/Main.java
+++ Area_Testes/Main.java
@@ -1,2 +1,16 @@
 package Area_Testes;
+import java.util.Scanner;
 
+public class Main{
+
+    static public void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nDigite um valor: ");
+        int A = scanner.nextInt();
+        int dobro = A * 2;
+        System.out.print("\nDobro do valor" + A + "é: " + dobro);
+
+        scanner.close();
+    }
+}

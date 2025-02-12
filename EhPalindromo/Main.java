4dc5abbc897106724a887c6f8a40490242df1362
Felipe Birchal Borges <felipebirchalborges@gmail.com>
2025-02-12 19:49:57 -0300

    .

--- EhPalindromo/Main.java
+++ EhPalindromo/Main.java
@@ -1,26 +1,22 @@
 package EhPalindromo;
 import java.util.Scanner;
 
-
 public class Main{
     static public void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        while(true)
+        String palavra = scanner.nextLine();
+        while(!palavra.equalsIgnoreCase("FIM"))
         {
-            String palavra = scanner.nextLine();
-            if(palavra.equalsIgnoreCase("FIM")){break;}
-            
             if(EhPalindromo(palavra)){System.out.println("SIM");}
             else {System.out.println("NÃO");}
+            palavra = scanner.nextLine();
         }
         scanner.close();
         
     }
     static boolean EhPalindromo(String palavra)
     {
-
-        palavra = palavra.replaceAll("\\s+", "").toLowerCase();
         int esq = 0;
         int dir = palavra.length() - 1;
         
